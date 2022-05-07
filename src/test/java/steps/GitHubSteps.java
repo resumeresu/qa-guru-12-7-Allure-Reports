cdf02cb31806a2f2d417b3ea67d263e9cd5cae3b
Svyatoslav Boytsov <sbo@tassta.com>
2022-05-07 16:22:20 +0300

    steps added

---
+++ src/test/java/steps/GitHubSteps.java
@@ -0,0 +1,44 @@
+package steps;
+
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubSteps {
+
+    @Step("Open GitHub")
+    public GitHubSteps openMainPage() {
+        open("https://github.com");
+        return this;
+    }
+
+    @Step("Search by given text: {text}")
+    public GitHubSteps searchByText(String text) {
+        $(".header-search-input").click();
+        $(".header-search-input").setValue(text).pressEnter();
+        return this;
+    }
+
+    @Step("Click the result by link text: {text}")
+    public GitHubSteps clickResult(String text) {
+        $(byLinkText(text)).click();
+        $("#issues-tab").click();
+        return this;
+    }
+
+    @Step("Open the Issues tab")
+    public GitHubSteps openIssuesTab() {
+        $("#issues-tab").click();
+        return this;
+    }
+
+    @Step("Check the issue name #{number} has title: {title}")
+    public GitHubSteps checkIssueNameHasText(String number, String title) {
+        $(byId("issue_" + number + "_link")).shouldHave(text(title));
+        return this;
+    }
+}
--- src/test/java/tests/GitHubTest.java
+++ src/test/java/tests/GitHubTest.java
@@ -1,33 +1,63 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
+import steps.GitHubSteps;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class GitHubTest {
 
+    static String searchText = "eroshenkoam/allure-example";
+    static String issueNumber = "76";
+    static String issueTitle = "С Новым Годом (2022)";
+
     @Test
     public void issueTest() {
-
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        String searchText = "eroshenkoam/allure-example";
-        String issueNumber = "76";
-        String issueTitle = "С Новым Годом (2022)";
-
         open("https://github.com");
+
         $(".header-search-input").click();
         $(".header-search-input").setValue(searchText).pressEnter();
         $(byLinkText(searchText)).click();
         $("#issues-tab").click();
-        $(byId("issue_" + issueNumber + "_link")).shouldHave(text(issueTitle));
 
+        $(byId("issue_" + issueNumber + "_link")).shouldHave(text(issueTitle));
     }
 
+    @Test
+    public void issueTestWithLambdas() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Open GitHub", () -> open("https://github.com"));
+        step("Search by given text: " + searchText, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").setValue(searchText).pressEnter();
+        });
+        step("Click the result", () -> $(byLinkText(searchText)).click());
+        step("Open the Issues tab", () -> $("#issues-tab").click());
+        step("Check the issue #" + issueNumber + " has title: " + searchText, () -> {
+            $(byId("issue_" + issueNumber + "_link")).shouldHave(text(issueTitle));
+        });
+    }
+
+    @Test
+    public void issueTestWithAnnotatedSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        GitHubSteps steps = new GitHubSteps();
+
+        steps.openMainPage()
+                .searchByText(searchText)
+                .clickResult(searchText)
+                .openIssuesTab()
+                .checkIssueNameHasText(issueNumber, issueTitle);
+
+    }
 }
